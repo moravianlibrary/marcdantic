5defadcbcfb22d1e489cbb7305ea42ed61a7303b
Robert Randiak <randiak@protonmail.com>
2025-03-19 18:25:01 +0100

    fix: comment out init version retrieving

--- marcdantic/__init__.py
+++ marcdantic/__init__.py
@@ -17,18 +17,18 @@ from .default_mapping import DEFAULT_MAPPING
 from .mapper import MarcMapper
 from .marc_record import MarcRecord
 
-import os
+# import os
 
 
-def read_version():
-    current_dir = os.path.dirname(__file__)
-    parent_dir = os.path.dirname(current_dir)
-    version_file_path = os.path.join(parent_dir, "VERSION")
-    with open(version_file_path, "r") as version_file:
-        return version_file.read().strip()
+# def read_version():
+#     current_dir = os.path.dirname(__file__)
+#     parent_dir = os.path.dirname(current_dir)
+#     version_file_path = os.path.join(parent_dir, "VERSION")
+#     with open(version_file_path, "r") as version_file:
+#         return version_file.read().strip()
 
 
-__version__ = read_version()
+# __version__ = read_version()
 
 
 __all__ = [
