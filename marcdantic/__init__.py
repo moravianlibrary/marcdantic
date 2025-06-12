3d116bc6067993fc7da9556dfcf236fbcc354535
Robert Randiak <randiak@protonmail.com>
2025-06-12 14:27:53 +0200

    fix: expose MarcIssue

--- marcdantic/__init__.py
+++ marcdantic/__init__.py
@@ -1,11 +1,13 @@
 from . import fieldsets, query
 from .default_mapping import DEFAULT_MAPPING
+from .fieldsets.issue import MarcIssue
 from .mapper import MarcMapper
 from .record import MarcRecord
 
 __all__ = [
     "DEFAULT_MAPPING",
     "fieldsets",
+    "MarcIssue",
     "MarcMapper",
     "MarcRecord",
     "query",
