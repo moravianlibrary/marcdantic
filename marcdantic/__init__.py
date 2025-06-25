51464b04039a34bf1b3c3e93075cf9c7729ce46a
Robert Randiak <randiak@protonmail.com>
2025-06-25 22:15:30 +0200

    chore: expose issuance type

--- marcdantic/__init__.py
+++ marcdantic/__init__.py
@@ -1,12 +1,13 @@
 from . import fieldsets, query
 from .default_mapping import DEFAULT_MAPPING
-from .fieldsets.issue import MarcIssue
+from .fieldsets.issue import IssuanceType, MarcIssue
 from .mapper import MarcMapper
 from .record import MarcRecord
 
 __all__ = [
     "DEFAULT_MAPPING",
     "fieldsets",
+    "IssuanceType",
     "MarcIssue",
     "MarcMapper",
     "MarcRecord",
