90cd8028d5e6dee3725f05d5d8f0d7325dbf11c9
Robert Randiak <randiak@protonmail.com>
2025-10-20 13:07:40 +0200

    fix: tag 006 not in control fields

--- marcdantic/constants.py
+++ marcdantic/constants.py
@@ -3,4 +3,4 @@ DIRECTORY_ENTRY_LENGTH = 12
 
 MARC_NS = {"marc": "http://www.loc.gov/MARC21/slim"}
 
-CONTROL_FIELDS = ["001", "003", "005", "007", "008", "009"]
+CONTROL_FIELDS = ["001", "003", "005", "006", "007", "008", "009"]
